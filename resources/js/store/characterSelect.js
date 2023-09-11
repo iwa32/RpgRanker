8bb626af38460f95b59b49a82fee93e217c58e8e
iwai32 <iwakou32@gmail.com>
2023-09-11 10:07:55 +0900

    update:indent整理

--- resources/js/store/characterSelect.js
+++ resources/js/store/characterSelect.js
@@ -1,49 +1,52 @@
-import { OK } from '../util.js'
+import { OK
+} from '../util.js'
 const characterSelect = {
-  namespaced: true,
-  state: {
-    characterList: [{}],
-    characterId: 1,
-    startCharacterNum: 0,
-    endCount: 3//キャラクター画面の最大表示数
-  },
-  getters: {
-    //キャラクターごとの配列インデックス
-    characterIndex(state) {
-      return state.characterId - 1
+    namespaced: true,
+    state: {
+        characterList: [{}],
+        characterId: 1,
+        startCharacterNum: 0,
+        endCount: 3 //キャラクター画面の最大表示数
     },
-    characterDisplayNumber(state) {
-      return state.characterList
-             .slice(state.startCharacterNum, state.endCount)
-    }
-  },
-  mutations: {
-    setCharacterList(state, data) {
-      state.characterList = data
+    getters: {
+        //キャラクターごとの配列インデックス
+        characterIndex(state) {
+            return state.characterId - 1
+        },
+        characterDisplayNumber(state) {
+            return state.characterList
+                .slice(state.startCharacterNum, state.endCount)
+        }
     },
-    selectedCharacter(state, characterId) {
-      state.characterId = characterId
+    mutations: {
+        setCharacterList(state, data) {
+            state.characterList = data
+        },
+        selectedCharacter(state, characterId) {
+            state.characterId = characterId
+        },
+        nextCharacter(state) {
+            state.startCharacterNum++
+            state.endCount++
+        },
+        prevCharacter(state) {
+            state.startCharacterNum--
+            state.endCount--
+        }
     },
-    nextCharacter(state) {
-      state.startCharacterNum++
-      state.endCount++
-    },
-    prevCharacter(state) {
-      state.startCharacterNum--
-      state.endCount--
-    }
-  },
-  actions: {
-    async getCharacterList({ commit }) {
-      const response = await axios.get('/api/character-list')
+    actions: {
+        async getCharacterList({
+            commit
+        }) {
+            const response = await axios.get('/api/character-list')
 
-      if (response.status !== OK) {
-        this.$store.commit('error/setCode', response.status)
-        return false
-      }
-      
-      commit('setCharacterList', response.data)
+            if (response.status !== OK) {
+                this.$store.commit('error/setCode', response.status)
+                return false
+            }
+
+            commit('setCharacterList', response.data)
+        }
     }
-  }
 }
 export default characterSelect
